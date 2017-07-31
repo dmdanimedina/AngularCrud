3b146529992174a807a6514ba7731ad6e80a37c3
Daniel Medina <dmdanimedina@gmail.com>
2017-07-31 16:05:34 -0400

    last part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,59 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title:string = 'Angular Crud';
+  msg:string="";
 
   employees =[
-    {'name':'fazt',position:'manager'},
-     {'name':'isaac',position:'manager'},
-      {'name':'flor',position:'designer'},
-       {'name':'pepe',position:'programer'},
-        {'name':'tania',position:'prorammer'}
+    {'name':'fazt',position:'manager',email:''},
+     {'name':'isaac',position:'manager',email:''},
+      {'name':'flor',position:'designer',email:''},
+       {'name':'pepe',position:'programer',email:''},
+        {'name':'tania',position:'prorammer',email:''}
   ];
 
+    employees2 =[];
+
   model:any={};
+  model2:any={};
+  hideUpdate:boolean = true;
 
   addEmployee():void{
-
+    this.employees.push(this.model);
+    this.msg="Campo Agregado";
+    this.model={};
   }
 
-  deleteEmployee():void{
-
+  deleteEmployee(i):void{
+      var answer = confirm('estas seguro de eliminar?');
+      if(answer){
+        this.employees.splice(i,1)
+        this.msg="Campo Eliminado";
+      }
   }
 
-  editEmployee():void{
-
+  myvalue;
+  editEmployee(i):void{
+   this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+     this.model2.email = this.employees[i].email;         
+     this.myvalue = i;
+     this.hideUpdate = false;
   }
 
   updateEmployee():void{
-
+      console.log(this.model2)
+      let i = this.myvalue;
+      for(let j=0 ; j<this.employees.length;j++){
+        if(i==j){
+          this.employees[i] = this.model2;
+          this.msg="Campo Actualizado";
+          this.hideUpdate = true;
+        }
+      }
+      this.model2={};
   }
 
+  closeAlert():void{
+    this.msg="";
+  }
 }
